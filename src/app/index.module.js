e06564bde67981ffe46225a84858b4392f4f9c79
Umair Jameel <umair@devigital.com>
2017-11-17 18:27:09 +0500

    first commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('myMessageFront', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
